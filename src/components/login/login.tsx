ee2a9a15c4e78674fb0e3b89660386976ca77049
Reynaldo Molina <molinareynaldo952@gmail.com>
2025-10-01 10:00:00 -0600

    Rename jero to hero to remove Hero with uppercase from github

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -17,7 +17,7 @@ import {
 } from '../ui/form';
 import Link from 'next/link';
 import { loginWithGoogle } from '@/server-actions/auth';
-import { Hero } from './jero';
+import { Hero } from './hero';
 
 export function LoginForm() {
   const form = useForm<z.infer<typeof loginSchema>>({
